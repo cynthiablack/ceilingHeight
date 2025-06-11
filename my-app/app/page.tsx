61390536cf93352eef3c02113cbdff2d30480126
Cynthia Black <cindy.black@gmail.com>
2025-06-11 02:34:21 -0800

    app/page skeleton

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,12 +5,21 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Ceiling Height Prediction Tool</h1>
+        <header>
+          <h1>Ceiling Height Prediction Tool</h1>
         <p>Please choose a date to display a prediction for Ted Stevens Anchorage International Airport (PANC)</p>
-        //date picker here
+        </header>
+        {/*interactive inputs */}
+        <section>
+          {/* TODO: date picker here*/}
+        </section>
+        {/*visualizations*/}
+        <section>
+          {/* TODO: graphs from PyMC here */}
+        </section>
       </main>
       <footer className={styles.footer}>
-        <p>Bayesian regression learning model created by Cynthia Black using pyMC &c 2025</p>
+        <p>Bayesian regression learning model created by Cynthia Black using PyMC &copy 2025</p>
       </footer>
     </div>
   );
