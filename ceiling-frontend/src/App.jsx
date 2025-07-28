ecb9c49adbe9c88c7ef75654bcd3e105c3082abd
Cynthia Black <cindy.black@gmail.com>
2025-07-27 16:26:03 -0800

    Date picker

--- ceiling-frontend/src/App.jsx
+++ ceiling-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [date, setDate] = useState('');
+
+          function DatePicker(date, setDate) {
+            return (
+              <input type="date" value={date} />
+            )
+          }
 
   return (
     <>
@@ -16,6 +20,7 @@ function App() {
       {/* interactive inputs */}
         <section>
           {/* TODO: date picker here */}
+          <DatePicker date={date} setDate={setDate} />
         </section>
         {/* visualizations */}
         <section>
